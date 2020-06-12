a505cfe6bfa7666c7a631d7e1a2909b121b168fe
Tisha-03 <65775280+Tisha-03@users.noreply.github.com>
2020-06-12 17:48:35 +0530

    Update Dictionary.py

--- Python_Task2/Dictionary.py
+++ Python_Task2/Dictionary.py
@@ -1,27 +1,27 @@
 #Initialize dictionary
-dict4 = {3:3, 1:1, 4:4}
-print(dict4)
+myDict = {3:3, 1:1, 4:4}
+print(myDict)
 #The keys() method returns a list of keys in a python dictionary
-print(dict4.keys())
+print(myDict.keys())
 #The values() method returns a list of values in the dictionry.
-print(dict4.values())
+print(myDict.values())
 #The items() method returns a list of key-value pairs.
-print(dict4.items())
+print(myDict.items())
 #The copy() method creates a shallow copy of the python dictionary.
-newdict = dict4.copy()
+newDict = myDict.copy()
 print(newdict)
 #The pop() method is used to remove and display an item from the dictionary
-print(newdict.pop(4))
-#Reassigning python dictionary newdict
-newdict = {3:3, 1:1, 4:4, 7:7, 9:9}
+print(newDict.pop(4))
+#Reassigning python dictionary newDict
+newDict = {3:3, 1:1, 4:4, 7:7, 9:9}
 #The popitem() method is used to pop pair.
-print(newdict.popitem())
+print(newDict.popitem())
 #The fromkeys() method creates a new Python dictionary from an existing one.
-print(newdict.fromkeys({1, 2, 3, 4, 7}, 0))
-#Initialize dictionaries dict1 and dict2
-dict1={1:1, 2:2}
-dict2={2:2, 3:3}
+print(newDict.fromkeys({1, 2, 3, 4, 7}, 0))
+#Initialize dictionaries Dict1 and Dict2
+Dict1={1:1, 2:2}
+Dict2={2:2, 3:3}
 #The update() method takes another dictionary as an argument.Then it updates the dictionary
-dict1.update(dict2)
-print(dict1)
+Dict1.update(Dict2)
+print(Dict1)
 

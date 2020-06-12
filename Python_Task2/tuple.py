6e97e327a4f56acba3bfe6fd64983a92dd282e59
Tisha-03 <65775280+Tisha-03@users.noreply.github.com>
2020-06-12 17:57:20 +0530

    Update tuple.py

--- Python_Task2/tuple.py
+++ Python_Task2/tuple.py
@@ -1,26 +1,26 @@
 #Initializing a Tuple
-z = (3, 7, 4, 2)
+tup_A = (3, 7, 4, 2)
 #Access the first item of a tuple at index 0
-print(z[0])
+print(tup_A[0])
 #Print last item in the tuple
-print(z[-1])
+print(tup_A[-1])
 #First index is inclusive (before the :) and last (after :) is not.
-print(z[0:2])
+print(tup_A[0:2])
 #Everything up to but not including index 3
-print(z[:3])
-#Initialize tuple1
-tup1 = ('Python', 'SQL')
-#Initialize another tuple2
-tup2 = ('R',)
-#Create new tuple based on addition of tuples tup1 and tup2
-new_tuple = tup1 + tup2;
-print(new_tuple)
+print(tup_A[:3])
+#Initialize tuple Tup1
+Tup1 = ('Python', 'SQL')
+#Initialize another tuple Tup2
+Tup2 = ('R',)
+#Create new tuple based on addition of tuples Tup1 and Tup2
+newTuple = Tup1 + Tup2;
+print(newTuple)
 #Initialize a new tuple
-animals = ('lama', 'sheep', 'lama', 48)
+tup_Animals = ('lama', 'sheep', 'lama', 48)
 #The index method returns the first index at which a value occurs
-print(animals.index('lama'))
+print(tup_Animals.index('lama'))
 #The count method returns the number of times a value occurs in a tuple
-print(animals.count('lama'))
+print(tup_Animals.count('lama'))
 #Iterate through a tuple
 for item in ('lama', 'sheep', 'lama', 48):
     print(item)
